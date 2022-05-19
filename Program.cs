899e2b417e2df6d5a09c081baef828e38ebd0102
Benton J <phixmeplease@gmail.com>
2022-05-18 23:10:05 -0400

    Add program base

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+    Higher/Lower Number game written in C#
+    Made so I can better understand the C# language.
+*/
+
+namespace HigherLower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started Game!");
+        }
+    }
+}
