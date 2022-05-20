08cb1c4d394723a00465186e34675212ad67e020
Benton J <phixmeplease@gmail.com>
2022-05-19 22:45:22 -0400

    Better-ify

--- Program.cs
+++ Program.cs
@@ -11,12 +11,20 @@ namespace HigherLower
     {
         static void Main(string[] args)
         {
-            HLGame game = new HigherLowerGame.HLGame(544);
+            Console.WriteLine("Guess a number 1-9999. We'll let you know if the number is higher or lower than your guess!\n");
+            Random random = new Random();
+            int num = random.Next(1, 9999);
+            HLGame game = new HigherLowerGame.HLGame(num);
+
+            Console.WriteLine(num);
 
             while (!game.GameStep())
             {}
 
             Console.WriteLine("You won!");
+
+            Console.WriteLine("Press any key to close!");
+            Console.ReadKey();
         }
     }
 }
